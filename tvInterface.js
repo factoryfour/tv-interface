91a68abedde70feac78a4a3daa166a1d99d93d88
Alex Mathews <alexjmathews@gmail.com>
2016-04-21 14:55:51 -0400

    updated interface

--- tvInterface.js
+++ tvInterface.js
@@ -1,6 +1,6 @@
 var request = require('request')
 
-module.exports = function(TV_API_KEY, TV_ACCOUNT_ID) {
+module.exports = function(TV_API_KEY, TV_ACCOUNT_ID, TV_ADMIN_VAULT_ID) {
   var TV_API_KEY_ENC = new Buffer(TV_API_KEY + ":").toString('base64');
   var TV_AUTH_HEADER = "Basic " + TV_API_KEY_ENC;
 
@@ -13,11 +13,14 @@ module.exports = function(TV_API_KEY, TV_ACCOUNT_ID) {
   /**
     Create a new Organization Vault
       pass: organization_JSON
-      Will create a new org vault and store ID in vault_id of organization_JSON
+      Will create a new org vault and store ID in vault of organization_JSON
       Name is senstive to organization.is_vendor
       callback(error, updated_organization_JSON)
     **/
   tvModule.createOrganizationVault = function(organization, callback) {
+    if (organization.id == '') {
+      return callback(Error("Organization must have an ID prior to being created"), null);
+    }
     var prefix = "cli_";
     if (organization.is_vendor) {
       prefix = "ven_"
@@ -37,8 +40,8 @@ module.exports = function(TV_API_KEY, TV_ACCOUNT_ID) {
     request(vaultCreateOptions, function(error, response, vaultCreatedBody) {
       if (error) return callback(Error(error), null);
       var vaultCreatedParsed = JSON.parse(vaultCreatedBody);
-      var vault_id = vaultCreatedParsed.vault.id;
-      organization.vault_id = vault_id;
+      var vault = vaultCreatedParsed.vault.id;
+      organization.vault = vault;
 
       return callback(null, organization);
     });
@@ -50,12 +53,12 @@ module.exports = function(TV_API_KEY, TV_ACCOUNT_ID) {
 
     var group_policy = [{
       Resources: [
-        "Vault::" + organization.vault_id + "::Document"
+        "Vault::" + organization.vault + "::Document"
       ],
       Activities: "RUD"
     }, {
       Resources: [
-        "Vault::" + organization.vault_id + "::Search::"
+        "Vault::" + organization.vault + "::Search::"
       ],
       Activities: "R"
     }];
@@ -82,6 +85,97 @@ module.exports = function(TV_API_KEY, TV_ACCOUNT_ID) {
       return callback(null, organization)
     });
   };
+
+  tvModule.addOrgPatientSchema = function(organization, callback) {
+
+    var patient_schema = {
+      name: "patient",
+      fields: [{
+        name: "first_name",
+        index: true,
+        type: "string"
+      }, {
+        name: "last_name",
+        index: true,
+        type: "string"
+      }, {
+        name: "clinic_id",
+        index: true,
+        type: "string"
+      }, {
+        name: "dob",
+        index: true,
+        type: "string"
+      }, {
+        name: "email",
+        index: true,
+        type: "string"
+      }, {
+        name: "phone",
+        index: true,
+        type: "string"
+      }, {
+        name: "address",
+        index: false,
+        type: "string"
+      }, {
+        name: "owner_id",
+        index: true,
+        type: "string"
+      }, {
+        name: "height",
+        index: true,
+        type: "float"
+      }, {
+        name: "weight",
+        index: true,
+        type: "float"
+      }, {
+        name: "notes",
+        index: true,
+        type: "string"
+      }, {
+        name: "needs_attention",
+        index: true,
+        type: "boolean"
+      }, {
+        name: "orders",
+        index: true,
+        type: "string"
+      }, {
+        name: "media",
+        index: true,
+        type: "string"
+      }]
+    }
+
+    var patient_schema_enc = new Buffer(JSON.stringify(patient_schema)).toString('base64');
+
+    var patientSchemaCreateOptions = {
+      method: 'POST',
+      url: 'https://api.truevault.com/v1/vaults/' + organization.vault + '/schemas',
+      headers: {
+        authorization: TV_AUTH_HEADER
+      },
+      formData: {
+        schema: patient_schema_enc
+      }
+    };
+
+    request(patientSchemaCreateOptions, function(error, response, schemaCreatedBody) {
+      if (error) return callback(Error(error), null);
+      var schemaCreatedBodyParsed = JSON.parse(schemaCreatedBody);
+      console.log(schemaCreatedBodyParsed)
+      var schema_id = schemaCreatedBodyParsed.schema.id;
+      organization.patient_schema = schema_id;
+      // console.log("Created group policy named " + policyCreatedParsed.group.name +" with ID " + group_policy_id);
+      return callback(null, organization)
+    });
+  };
+
+  tvModule.pushOrgDocument = function(organization, callback) {
+    return callback(null, "hello world");
+  }
 
   tvModule.test = function() {
     return "hello world";
