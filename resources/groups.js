37b4423308c40417af470a8b343bb0a2b386b1d6
Alex Mathews <alexjmathews@gmail.com>
2016-05-20 18:53:17 -0400

    updates should return true

--- resources/groups.js
+++ resources/groups.js
@@ -89,13 +89,12 @@ module.exports = function(TV_API_KEY_ENC, TV_AUTH_HEADER) {
         };
 
         request(options, function(error, response, body) {
-            if (error) return callback(Error(error), null);
+            if (error) return callback(Error(error), false);
             var bodyParsed = JSON.parse(body);
             if (bodyParsed.error) {
-                return callback(Error(bodyParsed.error.message), null);
+                return callback(Error(bodyParsed.error.message), false);
             }
-            var policy_id = bodyParsed.group.group_id;
-            return callback(null, policy_id)
+            return callback(null, true)
         });
     };
 
