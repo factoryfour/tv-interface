f43a178ef76ab15b97b82a2eed6ed21783445edd
Alex Mathews <alexjmathews@gmail.com>
2016-05-22 13:38:46 -0400

    user search as well

--- resources/search.js
+++ resources/search.js
@@ -34,10 +34,53 @@ module.exports = function(TV_API_KEY_ENC, TV_AUTH_HEADER) {
 
             // Parse and decode all documents
             var bodyParsed = JSON.parse(body);
+            if (bodyParsed.data) {
+                var docs = bodyParsed.data.documents;
+                if (search_option.full_document) {
+                    for (doc in docs) {
+                        docs[doc].document = JSON.parse(new Buffer(docs[doc].document, 'base64').toString('ascii'))
+                    }
+                }
+                return callback(null, bodyParsed.data.info, docs)
+            } else {
+                return callback(null, null, null);
+            }
+        });
+    };
+
+    /**
+     * search - Search for documents in a vault with a search option.
+     *
+     * @param  {String}     vault_id      vault to search in
+     * @param  {JSON}       search_option search option formatted a la Truevault
+     * @param  {function}   callback      function(error, info, documents)
+     */
+    tvModule.runUserQuery = function(search_option, callback) {
+        // Base 64 Encode Search Option
+        var search_option_enc = new Buffer(JSON.stringify(search_option)).toString('base64')
+
+        // Configure search options for particular vault
+        var options = {
+            method: 'POST',
+            url: 'https://api.truevault.com/v1/users/search',
+            headers: {
+                authorization: TV_AUTH_HEADER
+            },
+            formData: {
+                search_option: search_option_enc
+            }
+        };
+
+        request(options, function(error, response, body) {
+            if (error) return callback(Error(error));
+
+            // Parse and decode all documents
+            var bodyParsed = JSON.parse(body);
             var docs = bodyParsed.data.documents;
             if (search_option.full_document) {
+
                 for (doc in docs) {
-                    docs[doc].document = JSON.parse(new Buffer(docs[doc].document, 'base64').toString('ascii'))
+                    docs[doc].attributes = JSON.parse(new Buffer(docs[doc].attributes, 'base64').toString('ascii'))
                 }
             }
             return callback(null, bodyParsed.data.info, docs)
