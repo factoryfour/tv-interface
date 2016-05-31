329e1d6d22ffe0be247629ac2e74e607b3e635eb
Alex Mathews <alexjmathews@gmail.com>
2016-05-31 03:42:54 -0400

    forgot an error case

--- resources/documents.js
+++ resources/documents.js
@@ -131,11 +131,13 @@ module.exports = function(TV_API_KEY_ENC, TV_AUTH_HEADER) {
 
         request(options, function(error, response, body) {
             if (error) return callback(Error(error), false);
-            if (bodyParsed.error) {
-                return callback(Error(bodyParsed.error.message), false);
+            var result;
+            try {
+                result = JSON.parse(new Buffer(body, 'base64').toString('ascii'));
+                return callback(null, result)
+            } catch(e) {
+                return callback('Could not find document', null);
             }
-            var result = JSON.parse(new Buffer(body, 'base64').toString('ascii'));
-            return callback(null, result)
         });
     };
 
